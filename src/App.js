9ec6cdad4c86da0b1324b8f876da88705533793f
EyaOuenniche <eya.ouenniche@medtech.tn>
2024-04-23 17:20:51 +0100

    correct import error

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { SignIn } from './Pages/SignIn';
-import Dashboard from './components/Dashboard';
+import TableAdmin from './components/TableAdmin';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<SignIn />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<TableAdmin />} />
   
       </Routes>
     </Router>
