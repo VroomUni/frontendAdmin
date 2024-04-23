f14b3368fde1634a4d135357ab735075eb00f5b9
EyaOuenniche <eya.ouenniche@medtech.tn>
2024-04-23 21:29:47 +0100

    changes in login

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ function App() {
     
       <Router>
         <Routes>
-        <Route path="/home" element={<SignIn />} />
+      
           <Route path="/Dashboard" element={<Dashboard />} />
        
           <Route path="/Mainsection1" element={<MainSection1 />} />
-        <Route path="/dashboard" element={<TableAdmin />} />
+       
         <Route path="/table" element={<TableAdmin />} />
           <Route path="/Mainsection2" element={<MainSection2 />} />
           <Route path="/ "element={<SignIn/>} />
